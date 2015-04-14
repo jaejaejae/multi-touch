ec31e07974cc2447e37ee7f412298ee13b7b94b0
Li Yan <liyan920517@gmail.com>
2015-04-14 23:01:20 +0800

    results

--- resultListHandler.js
+++ resultListHandler.js
@@ -4,40 +4,92 @@ function parseResults(resultsObj){
 	removeExistingResults();
 	for (var i = 0; i < resultsObj.length; i++) {
 		var row = document.createElement("tr");
-		var tdInfo =  document.createElement("td");
-		var nameDiv =  document.createElement("div");
-		var attriDiv =  document.createElement("div");
-		var strongDiv = document.createElement("strong");
+		//append image div
+		row.appendChild(addFirstColumn(resultsObj[i]));
+		//append info div
+		row.appendChild(addSecondColumn(resultsObj[i]));
+		//append score div
+		row.appendChild(addThirdColumn(resultsObj[i]));
 
-		nameDiv.innerHTML = resultsObj[i].Name + "   " + resultsObj[i].ID;
-		nameDiv.classList.add("rankedItemInfo");
-
-		var tagsString = "";
-		for (var j = 0; j < resultsObj[i].Attributes.length; j++) {
-			tagsString += resultsObj[i].Attributes[j];
-			tagsString += "  ";
-		};
-
-		strongDiv.innerHTML = tagsString;
-		strongDiv.classList.add("price");
-		attriDiv.classList.add("priceBlock");
-		//append child
-		attriDiv.appendChild(strongDiv);
-		tdInfo.appendChild(nameDiv);
-		tdInfo.appendChild(attriDiv);
-		row.appendChild(tdInfo);
 		tbody.appendChild(row);
 	};
 }
 
-function addFirstColumn(resultsObj){
+//create and return the first column with image
+function addFirstColumn(result){
+	var tdImg =  document.createElement("td");
 	var firstDiv = document.createElement("div");
 	var image = document.createElement("img");
 
 	firstDiv.classList.add("rankedItemImage");
-	//image.src = resultsObj.img;
+	image.src = result.Img;
+
+	firstDiv.appendChild(image);
+	tdImg.appendChild(firstDiv);
+
+	return firstDiv;
+
 }
 
+
+function addSecondColumn(result){
+	var tdInfo =  document.createElement("td");
+	var nameDiv =  document.createElement("div");
+	var attriDiv =  document.createElement("div");
+	var strongDiv = document.createElement("strong");
+	
+
+	nameDiv.innerHTML = result.Name + "   " + result.ID;
+	nameDiv.classList.add("rankedItemInfo");
+
+	var tagsString = "";
+	var isMore = false;
+	for (var j = 0; j < result.Attributes.length; j++) {
+		if(j < 2){
+			tagsString += result.Attributes[j];
+			tagsString += "  ";
+		}
+		//if exceed 2, display more 
+		else if(j >= 2){
+			if(j == 2){
+				strongDiv.innerHTML = tagsString;
+				tagsString = "";
+			}
+			else{
+				isMore = true;
+				tagsString += result.Attributes[j];
+				tagsString += "  ";
+			}
+		}
+	};
+	strongDiv.classList.add("attributes");
+	attriDiv.appendChild(strongDiv);
+
+//add the more tag
+	if(isMore){
+		var moreInfo = document.createElement("a");
+		moreInfo.href = "#";
+		moreInfo.title = tagsString;
+		moreInfo.innerHTML = "more";
+		attriDiv.appendChild(moreInfo);
+	}
+
+	
+	
+	attriDiv.classList.add("attrBlock");
+	//append child	
+	tdInfo.appendChild(nameDiv);
+	tdInfo.appendChild(attriDiv);
+
+	return tdInfo;
+}
+
+function addThirdColumn (result) {
+	var tdScore = document.createElement("td");
+	tdScore.innerHTML = result.Score;
+
+	return tdScore;
+}
 
 function removeExistingResults(){
 	while (tbody.firstChild) {
